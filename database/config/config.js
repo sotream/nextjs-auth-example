d9538dd9e47a06e16c67c3725e94f8125ba7044d
Andrii Prisniiak <a.prisniak@wnb.com.ua>
2023-08-11 23:22:28 +0300

    chore(source): switch to hosted db

--- database/config/config.js
+++ database/config/config.js
@@ -5,13 +5,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     dialect: 'postgres',
     dialectModule: require('pg'),
-    host: process.env.POSTGRES_HOST,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
+    host: process.env.POSTGRES_HOST
   },
   production: {
     database: process.env.POSTGRES_DATABASE,
@@ -19,12 +13,6 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     dialect: 'postgres',
     dialectModule: require('pg'),
-    host: process.env.POSTGRES_HOST,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
+    host: process.env.POSTGRES_HOST
   }
 };
