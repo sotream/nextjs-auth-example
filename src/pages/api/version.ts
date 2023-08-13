552356c42b061135e2fee67856e7a5c9c274d77d
Andrii Prisniiak <a.prisniak@wnb.com.ua>
2023-08-13 17:30:55 +0300

    chore(source): improve api logic

--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 const version = (req: Request, res: Response): void => {
   switch (req.method) {
     case 'GET': {
-      res.status(200).json({ version: process.env.REACT_APP_VERSION || '0.0.1' });
+      res.status(200).json({ version: process.env.APP_VERSION || '0.0.1' });
 
       break;
     }
