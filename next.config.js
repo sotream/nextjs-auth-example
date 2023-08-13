cf188f948cb618285ad0f46095cedf299b07bdfc
Andrii Prisniiak <a.prisniak@wnb.com.ua>
2023-08-13 17:30:16 +0300

    chore(source): improve api logic

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
+/** @type {import("next").NextConfig} */
+const { i18n } = require("./next-i18next.config");
+const packageVersion = require("./package.json").version;
 
 module.exports = {
-    i18n,
-    reactStrictMode: false
-}
+  i18n,
+  reactStrictMode: false,
+  env: {
+    APP_VERSION: packageVersion
+  }
+};
